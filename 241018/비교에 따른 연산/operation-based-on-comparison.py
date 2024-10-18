f7359fe94933f3f0ad0098766126227883361848
BLACKY <132118240+nhd0918@users.noreply.github.com>
2024-10-18 13:59:44 +0900

    Title: 비교에 따른 연산, Time: 126ms, Memory: 24MB, Status: Passed - CodeTree

--- 241018/비교에 따른 연산/operation-based-on-comparison.py
+++ 241018/비교에 따른 연산/operation-based-on-comparison.py
@@ -1,8 +1,3 @@
 a, b = map(int, input().split())
 
-# 조건에 따라 결과 출력
-if a >= b:
-    print(a * b)  # a가 b보다 크면 두 수의 곱 출력
-else:
-    if b != 0:
-        print(a // b)  # b가 0이 아닐 때만 a를 b로 나눈 몫 출력
+print( a * b if a > b else b // a )
