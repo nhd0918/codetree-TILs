270bab3311c2e0968c6dc1655d4536a8f4a368ce
BLACKY <132118240+nhd0918@users.noreply.github.com>
2024-10-23 16:39:07 +0900

    Title: 두 사람, Time: 134ms, Memory: 24MB, Status: Passed - CodeTree

--- 241023/두 사람/two-person.py
+++ 241023/두 사람/two-person.py
@@ -1,4 +1,4 @@
 valueAge1, valueSex1 = input().split()
 valueAge2, valueSex2 = input().split() 
 
-print(  1 if ( int(valueAge1) >= 19 or int(valueAge2) >= 19 ) or ( valueSex1 == 'M' or valueSex2 == 'M' ) else 0)
+print(  1 if ( int(valueAge1) >= 19 and valueSex1 == 'M' ) or ( int(valueAge2) >= 19 and valueSex2 == 'M' ) else 0)
