500f6ad566068e1bd2f1d2ae06dc342e9a333308
BLACKY <132118240+nhd0918@users.noreply.github.com>
2024-10-23 16:03:40 +0900

    Title: 숫자의 계절은, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 241023/숫자의 계절은/season-of-num.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+if n in [3,4,5]:
+    print('Spring')
+elif n in [6,7,8]:
+    print('Summer')
+elif n in [9,10,11]:
+    print('Fall')
+elif n in [12,1,2]:
+    print('Winter')
