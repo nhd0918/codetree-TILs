199c01fcebb192c7f57010948e6d38e9443416ca
BLACKY <132118240+nhd0918@users.noreply.github.com>
2024-10-23 15:26:50 +0900

    Title: 특정 조건 세 정수 비교, Time: 143ms, Memory: 26MB, Status: Passed - CodeTree

--- 241023/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
+++ 241023/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
@@ -1,10 +1,2 @@
 a, b, c = map(int, input().split())
-
-# a, b, c -> a : 1 -> b, c 
-
-minValues = min(a,b,c)
-
-f = 1 if a == minValues else 0
-s = 1 if a == b and a == c and b == c else 0
-
-print(f"{f} {s}")
+print(int(a == min(a, b, c)), int(a == b == c))
