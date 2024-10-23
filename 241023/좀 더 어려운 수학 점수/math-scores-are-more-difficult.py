f1ef7d08ec67f293611c98dd6b7f43853c1c4738
BLACKY <132118240+nhd0918@users.noreply.github.com>
2024-10-23 16:22:14 +0900

    Title: 좀 더 어려운 수학 점수, Time: 126ms, Memory: 24MB, Status: Passed - CodeTree

--- 241023/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
+++ 241023/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
@@ -2,13 +2,4 @@
 mathValueA, engValueA = map(int, input().split())
 mathValueB, engValueB = map(int, input().split())
 
-
-if mathValueA > mathValueB:
-    print('A')
-elif mathValueA < mathValueB:
-    print('B')
-elif mathValueA == mathValueB:
-    if engValueA > engValueB:
-        print('A')
-    elif engValueA < engValueB:
-        print('B')
+print('A' if (mathValueA > mathValueB) or (mathValueA == mathValueB and engValueA > engValueB) else 'B')
